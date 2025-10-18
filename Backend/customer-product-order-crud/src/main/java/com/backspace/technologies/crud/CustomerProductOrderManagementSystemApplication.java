30782c0448a5705aa50b2ed6f6c91aed45894884
enikma-mod <sirmodika@gmail.com>
2025-10-18 09:55:50 +0200

    project setup

---
+++ Backend/customer-product-order-crud/src/main/java/com/backspace/technologies/crud/CustomerProductOrderManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.backspace.technologies.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerProductOrderManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerProductOrderManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Backend/customer-product-order-crud/src/test/java/com/backspace/technologies/crud/CustomerProductOrderManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backspace.technologies.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerProductOrderManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
